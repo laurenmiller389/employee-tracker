e09f47416aa402fda91e689ddabeaeda43ed7507
Lauren Miller <lmiller90@fordham.edu>
2023-06-23 14:27:22 -0400

    added index installs and established db connection

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer")
+const mysql = require("mysql2")
+
+const db = mysql.createCOnnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'laurensql389',
+        database: 'employees_db'
+    },
+    console.log("connected to the movie_db database")
+);
