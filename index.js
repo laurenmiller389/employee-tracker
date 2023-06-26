25cfa85bde06920e815dea918971163337776ec3
Lauren Miller <lmiller90@fordham.edu>
2023-06-26 19:42:51 -0400

    moved connection point

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql2")
 
-const db = mysql.createCOnnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'laurensql389',
-        database: 'employees_db'
-    },
-    console.log("connected to the movie_db database")
-);
+
+
+//make inquirer prompt screen
+
+function init() {
+    inquirer.createPromptModule(
+        [
+            {
+                "message": "what would you like to do?",
+                "name": "choice",
+                "type": "list",
+                "choices": ["view movies", "add movies"]
+            }
+        ]
+    ).then(answer => {
+        if(answer.choice === "view movies") {
+            //return all the movies in the dv
+            viewAllMovies().then(([rows]) => console.log(rows)).then(() => init());
+        }
+    })
+}
+
+init();
+
+function viewAllMovies() {
+    const sql = `SELECT id, movies_name AS title FROM movies`;
+    return db.promise().query(sql);
+    };
